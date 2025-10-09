d48df142e044ec10e5129889212c114582050553
atacansuder <53799209+atacansuder@users.noreply.github.com>
2025-10-09 02:01:29 +0200

    Add furigana generation and removal buttons to editor menu

--- main.ts
+++ main.ts
@@ -33,45 +33,81 @@ export default class ObsidianFuriganaGenerator extends Plugin {
 			id: "add-furigana-to-selected-text",
 			name: "Add furigana to selected text",
 			editorCallback: async (editor: Editor) => {
-				const selection = editor.getSelection();
-				const selectionWithFurigana =
-					await this.furiganaService.generateFurigana(selection);
-				editor.replaceSelection(selectionWithFurigana);
+				await this.addFuriganaToSelection(editor);
 			},
 		});
 
-		this.addCommand({
-			id: "add-furigana-to-entire-document",
-			name: "Add furigana to entire document",
-			editorCallback: async (editor: Editor) => {
-				const content = editor.getValue();
-				const contentWithFurigana =
-					await this.furiganaService.generateFurigana(content);
-				editor.setValue(contentWithFurigana);
-			},
-		});
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu, editor) => {
+				menu.addItem((item) => {
+					item.setTitle("Add furigana to selection")
+						.setIcon("japanese-yen")
+						.onClick(async () => {
+							await this.addFuriganaToSelection(editor);
+						});
+				});
+			})
+		);
 
 		this.addCommand({
 			id: "remove-furigana-from-selected-text",
 			name: "Remove furigana from selected text",
 			editorCallback: async (editor: Editor) => {
-				const selection = editor.getSelection();
-				const selectionWithoutFurigana =
-					await this.furiganaService.removeFurigana(selection);
-				editor.replaceSelection(selectionWithoutFurigana);
+				await this.removeFuriganaFromSelection(editor);
 			},
 		});
+
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu, editor) => {
+				menu.addItem((item) => {
+					item.setTitle("Remove furigana from selection")
+						.setIcon("japanese-yen")
+						.onClick(async () => {
+							await this.removeFuriganaFromSelection(editor);
+						});
+				});
+			})
+		);
+
+		this.addCommand({
+			id: "add-furigana-to-entire-document",
+			name: "Add furigana to entire document",
+			editorCallback: async (editor: Editor) => {
+				await this.addFuriganaToDocument(editor);
+			},
+		});
+
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu, editor) => {
+				menu.addItem((item) => {
+					item.setTitle("Add  furigana to document")
+						.setIcon("japanese-yen")
+						.onClick(async () => {
+							await this.addFuriganaToDocument(editor);
+						});
+				});
+			})
+		);
 
 		this.addCommand({
 			id: "remove-furigana-from-entire-document",
 			name: "Remove furigana from entire document",
 			editorCallback: async (editor: Editor) => {
-				const content = editor.getValue();
-				const contentWithoutFurigana =
-					await this.furiganaService.removeFurigana(content);
-				editor.setValue(contentWithoutFurigana);
+				await this.removeFuriganaFromDocument(editor);
 			},
 		});
+
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu, editor) => {
+				menu.addItem((item) => {
+					item.setTitle("Remove furigana from document")
+						.setIcon("japanese-yen")
+						.onClick(async () => {
+							await this.removeFuriganaFromDocument(editor);
+						});
+				});
+			})
+		);
 	}
 
 	onunload() {}
@@ -86,5 +122,34 @@ export default class ObsidianFuriganaGenerator extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+
+	async addFuriganaToSelection(editor: Editor) {
+		const selection = editor.getSelection();
+		const selectionWithFurigana =
+			await this.furiganaService.generateFurigana(selection);
+		editor.replaceSelection(selectionWithFurigana);
+	}
+
+	async addFuriganaToDocument(editor: Editor) {
+		const content = editor.getValue();
+		const contentWithFurigana = await this.furiganaService.generateFurigana(
+			content
+		);
+		editor.setValue(contentWithFurigana);
+	}
+
+	async removeFuriganaFromSelection(editor: Editor) {
+		const selection = editor.getSelection();
+		const selectionWithoutFurigana =
+			await this.furiganaService.removeFurigana(selection);
+		editor.replaceSelection(selectionWithoutFurigana);
+	}
+
+	async removeFuriganaFromDocument(editor: Editor) {
+		const content = editor.getValue();
+		const contentWithoutFurigana =
+			await this.furiganaService.removeFurigana(content);
+		editor.setValue(contentWithoutFurigana);
 	}
 }
